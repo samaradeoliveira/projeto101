fa4c9cc2a16f361f0a9aff857fdfb361c92ba07b
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2024-01-26 14:51:18 -0300

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,39 @@
+import random
+
+response = "y"
+
+#while response == "A":
+ ##   no = random.randint(1, 6)
+    # if no == 1:
+  ##  print("[-----]")
+   ## print("[     ]")
+   ## print("[  "+str(no)+"  ]")  # "[  0  ]"
+   ## print("[     ]")
+   ## print("[-----]")
+
+    ##response = input("Quer jogar o dado novamente?(y/n): ")
+    #"Pressione y para jogar novamente e n para sair:"
+
+#while response == "y":
+ ##   no = random.randint(1, 6)
+    # if no == 1:
+  ##  print("[-----]")
+   ## print("[     ]")
+   ## print("[  "+str(no)+"  ]")  # "[  0  ]"
+   ## print("[     ]")
+   ## print("[-----]")
+
+    ##response = input("Quer jogar o dado novamente?(y/n): ")
+    #"Pressione y para jogar novamente e n para sair:"
+
+#while response == "x":
+ ##   no = random.randint(1, 6)
+    # if no == 1:
+  ##  print("[-----]")
+   ## print("[     ]")
+   ## print("[  "+str(no)+"  ]")  # "[  0  ]"
+   ## print("[     ]")
+   ## print("[-----]")
+
+    ##response = input("Quer jogar o dado novamente?(y/n): ")
+    #"Pressione y para jogar novamente e n para sair:"
